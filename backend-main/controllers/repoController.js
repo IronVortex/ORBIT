7711bb31feca365d6b166c8fac5866a7bbbdb5a7
unknown <ajafshan17@gmail.com>
2026-09-20 23:29:49 +0530

    handle empty repository state

--- backend-main/controllers/repoController.js
+++ backend-main/controllers/repoController.js
@@ -84,11 +84,12 @@ async function fetchRepositoriesForCurrentUser(req, res) {
   try {
     const repositories = await Repository.find({ owner: userID });
 
-    if (!repositories || repositories.length == 0) {
-      return res.status(404).json({ error: "User Repositories not found!" });
-    }
     console.log(repositories);
-    res.json({ message: "Repositories found!", repositories });
+
+    res.json({
+      message: "Repositories found!",
+      repositories,
+    });
   } catch (err) {
     console.error("Error during fetching user repositories : ", err.message);
     res.status(500).send("Server error");
